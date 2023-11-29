f8566b65b21692620d6d4591b11ab6fe4b20500d
yuvrajgadadare <42262087+yuvrajgadadare@users.noreply.github.com>
2023-11-29 14:11:23 +0530

    Initial commit

---
+++ SecondProject/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondProject
+{
+    internal class Program
+    {
+        public void Square()
+        {
+            int a = 10, b;
+            b = a * a;
+            Console.WriteLine("Square=" + b);
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Square();
+            Console.ReadLine();
+        }
+    }
+}
